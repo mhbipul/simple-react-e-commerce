99a131487b30e4ad64ae6b0d7e377f227554e29f
Bipul <mhbipul99bd@gmail.com>
2024-03-03 23:37:22 +0600

    header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <a href="/shop"> Shop </a>
+        <a href="/order"> Order </a>
+        <a href="/inventoroy"> Inventory </a>
+        <a href="/login"> Login </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
