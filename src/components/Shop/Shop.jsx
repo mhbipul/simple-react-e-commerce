c6144156365c61632f240f813e2e8385082a47ad
Bipul <mhbipul99bd@gmail.com>
2024-03-15 15:40:47 +0600

    Added Products

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,35 @@
+import  { useEffect, useState  } from 'react'
+import "./Shop.css"
+import Product from '../Product/Product';
+
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+    
+
+    useEffect(() => {
+        fetch("https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json")
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+
+
+  return (
+    <div className='shop-container'>
+      <div className="products-container">
+         {
+          products.map(product => <Product   
+            key={product.id}
+            product={product}
+          />)
+         }
+
+      </div>
+      <div className="cart-container">
+        <h3> Order Summary  </h3>
+      </div>
+    </div>
+  )
+}
+
+export default Shop
